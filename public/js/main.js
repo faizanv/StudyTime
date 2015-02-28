c4bcafb1116f9e960d013c5b0f453660bb1bec2d
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-02-28 12:02:21 -0600

    added some skeleton view

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    // var cardList = getCards();
+    	//append div Card to cardList
+    	//depending on size only load certain amount of cards
+    		//load more if user scrolls (event listener)
+    // if cardList != empty
+    	// showDetail()
+    // else
+    	// show friendly prompt
+    //(left side) card onClick()/showDetail() puts it on the right side $().add()
+    //new card button exopses a form
+    //save card button posts if unique card and puts(updates) if not
+
+});
